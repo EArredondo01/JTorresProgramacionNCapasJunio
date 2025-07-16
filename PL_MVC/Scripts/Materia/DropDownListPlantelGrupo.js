a76941eb0cc26ce631180f239b68f446c614c045
jtorrestorres <jtorrstorrs@gmail.com>
2025-07-16 17:49:39 -0600

    JS y jQuery

---
+++ PL_MVC/Scripts/Materia/DropDownListPlantelGrupo.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    $('#txtNombre').blur(function (e) {
+        var Resultado = $(this).val()
+        alert(Resultado);
+
+
+    })
+
+});
+
+function MostrarTexto() {
+    var Resultado = document.getElementById("txtNombre").value //JavaScript //Id
+    var Resultado2 = $("#txtNombre").val(); //1
+
+    alert(Resultado);
+}
+
+function GetElemets() {
+    var Resultado2 = $(".form-control"); //1
+
+}
+
