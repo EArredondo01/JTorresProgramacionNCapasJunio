8741db6dfd8b845963863e1f87dd4d575f7c28e6
jtorrestorres <jtorrstorrs@gmail.com>
2025-07-24 14:14:57 -0600

    1 BeginForm en Guardar imagen y guardar usuario

--- PL_MVC/Controllers/MateriaController.cs
+++ PL_MVC/Controllers/MateriaController.cs
@@ -36,13 +36,13 @@ namespace PL_MVC.Controllers
         public ActionResult DeleteImagen(string IdImagenMateria) //Action Method 
         {
             ML.ImagenMateria imagenMateria = new ML.ImagenMateria();
-            if((Session["ListaImagenes"]) != null)
+            if ((Session["ListaImagenes"]) != null)
             {
                 imagenMateria.ImagenesMaterias = (Session["ListaImagenes"]) as List<object>;
 
                 foreach (ML.ImagenMateria obj in imagenMateria.ImagenesMaterias)
                 {
-                    if(obj.IdImagenMateria == IdImagenMateria)
+                    if (obj.IdImagenMateria == IdImagenMateria)
                     {
                         imagenMateria.ImagenesMaterias.Remove(obj);
                         break;
@@ -51,14 +51,14 @@ namespace PL_MVC.Controllers
 
                 int x;
 
-                bool PudoConvertir = int.TryParse(IdImagenMateria,out x);
+                bool PudoConvertir = int.TryParse(IdImagenMateria, out x);
 
-                if(PudoConvertir==true)
+                if (PudoConvertir == true)
                 {
                     //BL.   ImagenMateria.Delete(IdImagenMateria);
                 }
-                
-                
+
+
 
                 Session["ListaImagenes"] = imagenMateria.ImagenesMaterias;
 
@@ -68,54 +68,24 @@ namespace PL_MVC.Controllers
                 ViewBag.Error = "No existen elementos en la sesión";
             }
 
-            return RedirectToAction("Form", new { IdMateria = 0 });
+            return RedirectToAction("Form", new { IdMateria = 0, BorrarSesion = false });
 
         }
+        //[HttpPost]
+        //public ActionResult AddImagen(ML.Materia materia) //Action Method 
+        //{
 
-        [HttpPost]
-        public ActionResult AddImagen(ML.Materia materia) //Action Method 
-        {
-
-            materia.ImagenMateria.ImagenesMaterias = new List<object>();
-
-            HttpPostedFileBase imagen = Request.Files["imgMateriaInput"];
-
-            #region ConvertStreamToByte
-            if (imagen.ContentLength > 0)
-            {
-                MemoryStream target = new MemoryStream();
-                imagen.InputStream.CopyTo(target);
-                byte[] data = target.ToArray();
-                materia.ImagenMateria.Imagen = data;
-                materia.ImagenMateria.IdImagenMateria = Guid.NewGuid().ToString();
-                materia.ImagenMateria.Nombre = imagen.FileName;
-            }
-            #endregion 
-
-
-
-            if (Session["ListaImagenes"] == null)
-            {
-                materia.ImagenMateria.ImagenesMaterias.Add(materia.ImagenMateria);
-            }
-            else
-            {
-                materia.ImagenMateria.ImagenesMaterias = (Session["ListaImagenes"]) as List<object>;
-                materia.ImagenMateria.ImagenesMaterias.Add(materia.ImagenMateria);
-            }
-
-            
-
-            Session["ListaImagenes"] = materia.ImagenMateria.ImagenesMaterias;
-
-            return RedirectToAction("Form", new { IdMateria = materia.IdMateria, BorrarSesion = false });
-        }
+        //}
 
 
         [HttpGet] // Mostrar la vista
         public ActionResult Form(int? IdMateria, bool? BorrarSesion) //Add, update
         {
             ML.Materia materia = new ML.Materia();
+            if (Session["ModelMateria"] != null)
+            {
+                materia = (ML.Materia)Session["ModelMateria"];
+            }
             materia.ImagenMateria = new ML.ImagenMateria();
 
             ML.Result resultSemestres = new ML.Result();
@@ -215,44 +185,87 @@ namespace PL_MVC.Controllers
         }
 
         [HttpPost] // Recibir datos, guardar datos
-        public ActionResult Form(ML.Materia materia) //Add, update
+        public ActionResult Form(ML.Materia materia, string Action) //CargarImagenes, GuardarMateria
         {
-            if (materia.IdMateria == 0) //Add
+            if(Action == "GuardarUsuario")
             {
-                ML.Result result = BL.Materia.AddLinq(materia);
-
-                int IdMateria = (int)result.Object;
-
-
-                if(IdMateria> 0)
+                if (materia.IdMateria == 0) //Add
                 {
-                    //insertar las materia
-                    materia.ImagenMateria = new ML.ImagenMateria();
-                    materia.ImagenMateria.ImagenesMaterias = (Session["ListaImagenes"]) as List<object>;
+                    ML.Result result = BL.Materia.AddLinq(materia);
+
+                    int IdMateria = (int)result.Object;
 
 
-                    foreach (ML.ImagenMateria imagenMateriaObj in materia.ImagenMateria.ImagenesMaterias)
+                    if (IdMateria > 0)
                     {
-                        materia.ImagenMateria = imagenMateriaObj;
-                        materia.IdMateria = IdMateria;
-                        
-                        //if(Convert.ToInt(imagenMateriaObj.IdImagenMateria))
-                        //{
-                        //    ML.Result resultInsertImagenMateria = BL.ImagenMateria.Update(materia);
+                        //insertar las materia
+                        materia.ImagenMateria = new ML.ImagenMateria();
+                        materia.ImagenMateria.ImagenesMaterias = (Session["ListaImagenes"]) as List<object>;
 
-                        //}
-                        //else
-                        //{
+
+                        foreach (ML.ImagenMateria imagenMateriaObj in materia.ImagenMateria.ImagenesMaterias)
+                        {
+                            materia.ImagenMateria = imagenMateriaObj;
+                            materia.IdMateria = IdMateria;
+
+                            //if(Convert.ToInt(imagenMateriaObj.IdImagenMateria))
+                            //{
+                            //    ML.Result resultInsertImagenMateria = BL.ImagenMateria.Update(materia);
+
+                            //}
+                            //else
+                            //{
                             ML.Result resultInsertImagenMateria = BL.ImagenMateria.Add(materia);
 
-                        //}
+                            //}
+                        }
                     }
                 }
+                else //Update
+                {
+
+                }
             }
-            else //Update
+            else
             {
 
+                materia.ImagenMateria.ImagenesMaterias = new List<object>();
+
+                HttpPostedFileBase imagen = Request.Files["imgMateriaInput"];
+
+                #region ConvertStreamToByte
+                if (imagen.ContentLength > 0)
+                {
+                    MemoryStream target = new MemoryStream();
+                    imagen.InputStream.CopyTo(target);
+                    byte[] data = target.ToArray();
+                    materia.ImagenMateria.Imagen = data;
+                    materia.ImagenMateria.IdImagenMateria = Guid.NewGuid().ToString();
+                    materia.ImagenMateria.Nombre = imagen.FileName;
+                }
+                #endregion
+
+
+
+                if (Session["ListaImagenes"] == null)
+                {
+                    materia.ImagenMateria.ImagenesMaterias.Add(materia.ImagenMateria);
+                }
+                else
+                {
+                    materia.ImagenMateria.ImagenesMaterias = (Session["ListaImagenes"]) as List<object>;
+                    materia.ImagenMateria.ImagenesMaterias.Add(materia.ImagenMateria);
+                }
+
+
+
+                Session["ListaImagenes"] = materia.ImagenMateria.ImagenesMaterias;
+                Session["ModelMateria"] = materia;
+                return RedirectToAction("Form", new { IdMateria = materia.IdMateria, BorrarSesion = false });
+
+                
             }
+
 
 
             return RedirectToAction("GetAll");
